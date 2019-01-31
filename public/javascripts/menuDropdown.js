6c2f40a96947fdd3e059eb5180187da2a8cf049f
Zac Crosby <Zacros7164@gmail.com>
2019-01-31 11:45:58 -0500

    added navbar basics

---
+++ public/javascripts/menuDropdown.js
@@ -0,0 +1,18 @@
+function menuDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    // $('#myDropdown').show();
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+}
