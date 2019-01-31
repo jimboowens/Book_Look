5c032c827fbec78b6f0c0378f438a471d5778534
“jimboowens” <jimcowens@gmail.com>
2019-01-31 14:48:09 -0500

    added some ejs files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',(req, res, next)=> {
+  res.render('index', {});
 });
+
+router.get('/login',(req,res)=>{
+  // res.json(req.body);
+  res.render('login', {});
+
+});
+
+router.post('/loginProcess',[],(req,res)=>{
+  res.json(req.params.body)
+})
 
 module.exports = router;
